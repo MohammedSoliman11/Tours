4288a866e401446ed150e0aad1fdc211b016b16d
MohammedSoliman11 <mido2000068@gmail.com>
2023-12-11 21:50:32 +0200

    adding getTourStatus and getMonthlyPlan routes

--- routes/tourRouters.js
+++ routes/tourRouters.js
@@ -7,9 +7,13 @@ const {
   updateTour,
   removeTour,
   top5Cheap,
+  getTourStat,
+  getMonthlyPlan,
 } = require(`./../controllers/tourController`);
 const router = express.Router();
 
+router.route('/get-stats').get(getTourStat);
+router.route('/get-monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-cheap').get(top5Cheap, getAllTours);
 router.route('/').get(getAllTours).post(addTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(removeTour);
